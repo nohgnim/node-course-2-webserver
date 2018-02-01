cf98ed62bdb8afc9eaf0fff264da01475900b3c9
minghon <mhon@clearstreaminc.com>
2018-01-31 17:25:17 -0700

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const hbs = require('hbs') //Express handlebars engine
 const fs = require('fs')
+const port = process.env.PORT || 3000
+
 let app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -61,5 +63,5 @@ app.get('/bad', (req, res) => {
     })
 })
 app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+    console.log(`Server is up on port ${port}`)
 })
